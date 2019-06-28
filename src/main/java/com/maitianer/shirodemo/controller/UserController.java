29aba874f50b4d46be8440f1c4f954134d01612b
kuangzhan2014 <742598832@qq.com>
2019-06-28 11:33:44 +0800

    用户登录认证完成

--- src/main/java/com/maitianer/shirodemo/controller/UserController.java
+++ src/main/java/com/maitianer/shirodemo/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.maitianer.shirodemo.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.security.auth.Subject;
 
 /**
  * @Author: zhou
@@ -35,5 +39,31 @@ public class UserController {
     @RequestMapping("/toLogin")
     public String tologin(){
         return "/login";
+    }
+
+    @RequestMapping("/login")
+    public String login(String username,String password,Model model){
+        System.out.println("username="+username);
+        /**
+         * 使用shiro编写认证操作
+         * */
+        //1.获取Subject
+        org.apache.shiro.subject.Subject subject =SecurityUtils.getSubject();
+        //2.封装用户数据
+        UsernamePasswordToken token=new UsernamePasswordToken(username,password);
+        //3.执行登录方法
+        try {
+            //登录成功
+            subject.login(token);
+            return "test";
+        } catch (UnknownAccountException e) {
+            //登录失败:用户名不存在
+            model.addAttribute("msg","用户名不存在");
+            return "login";
+        } catch (IncorrectCredentialsException e) {
+            //登录失败:密码错误
+            model.addAttribute("msg","密码错误");
+            return "login";
+        }
     }
 }
